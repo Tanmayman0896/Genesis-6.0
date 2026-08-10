5fc3fbd9d6b6ed1ec3d34bb4962bc5ddc41f69b6
Ronit Mahajan <ronitmahajann270@gmail.com>
2026-08-10 23:23:47 +0530

    small si mistake hehe

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "prismic-io.s3.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "pub-2b91df05320148438318902a8dc7795b.r2.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "*.r2.dev",
+      },
     ],
   },
 };
